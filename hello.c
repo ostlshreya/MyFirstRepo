a634b326923c6042cc56758922b4d984b2e17337
ostlshreya <114209108+ostlshreya@users.noreply.github.com>
2022-09-23 10:46:17 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int n;
+	printf("Hello World\n");
+	printf("Enter number:\n");
+	scanf("%d", &n);
+	n=n+1;
+	printf("The number is %d\n" , n);
+	
+	exit(0);
+}
